f4066f246f27d00fd2b21fc15c41c9cefc85e07a
Kousheek Chakraborty <kousheekc@gmail.com>
2023-10-18 13:41:40 +0200

    pan and zoom navigation

--- graphics_view.py
+++ graphics_view.py
@@ -1,4 +1,3 @@
-import typing
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtCore import *
@@ -8,10 +7,14 @@ class GraphicsView(QGraphicsView):
     def __init__(self, scene):
         super().__init__()
 
+        self.zoomInFactor = 1.25
+        self.zoomClamp = True
+        self.zoom = 10
+        self.zoomStep = 1
+        self.zoomRange = [0, 10]
+
         self.scene = scene
-
         self.initUI()
-
         self.setScene(self.scene)
 
     def initUI(self):
@@ -21,23 +24,32 @@ class GraphicsView(QGraphicsView):
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
     def mousePressEvent(self, event):
-        if event.button() == Qt.RightButton:
-            self.middleMouseButtonPress(event)
-        else:
-            super().mousePressEvent(event)
+        if event.button() == Qt.LeftButton:
+            self.setDragMode(QGraphicsView.ScrollHandDrag)
+        super().mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.RightButton:
-            self.middleMouseButtonRelease(event)
+        self.setDragMode(QGraphicsView.NoDrag)
+        super().mouseReleaseEvent(event)
+
+    def wheelEvent(self, event: QWheelEvent):
+        zoomOutFactor = 1 / self.zoomInFactor
+
+        if event.angleDelta().y() > 0:
+            zoomFactor = self.zoomInFactor
+            self.zoom += self.zoomStep
         else:
-            super().mouseReleaseEvent(event)
+            zoomFactor = zoomOutFactor
+            self.zoom -= self.zoomStep
 
-    def middleMouseButtonPress(self, event):
-        releaseEvent = QMouseEvent(QEvent.MouseButtonRelease, event.localPos(), event.screenPos(), Qt.RightButton, Qt.NoButton, event.modifiers())
-        super().mouseReleaseEvent(releaseEvent)
-        self.setDragMode(QGraphicsView.ScrollHandDrag)
-        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(), Qt.RightButton, event.buttons() | Qt.LeftButton, event.modifiers())
-        super().mousePressEvent(fakeEvent)
 
-    def middleMouseButtonRelease(self, event):
-        print("middle mouse button released")
+        clamped = False
+        if self.zoom < self.zoomRange[0]: self.zoom, clamped = self.zoomRange[0], True
+        if self.zoom > self.zoomRange[1]: self.zoom, clamped = self.zoomRange[1], True
+
+        if not clamped or self.zoomClamp is False:
+            self.scale(zoomFactor, zoomFactor)
+
+
+
+
